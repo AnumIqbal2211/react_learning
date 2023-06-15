c423b9e453f51a1262570f8345a33b3358efe41d
Anum Iqbal <anumiqbal2211@gmail.com>
2023-06-16 00:59:31 +0500

    Implemented react rendering element example

---
+++ src/components/ReactCompAndProp.js
@@ -0,0 +1,16 @@
+function Info(data) {
+  return (
+    <div>
+      <h1>{data.name}</h1>
+    </div>
+  );
+}
+
+function Res() {
+  return (
+    <div>
+      <Info name="Anum" />
+    </div>
+  );
+}
+export default Res;
